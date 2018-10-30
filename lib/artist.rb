62fc50c78a21f85622f26a88edc41ae4afd8fb79
Vince Andrew Pimentel <contact@vincep.me>
2018-10-30 18:49:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,10 +29,10 @@ class Artist
     @@all << self
   end
 
-  # def add_song(song)
-  #   @songs << song
-  #   song.artist = self
-  # end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
 
   def songs
     @songs
